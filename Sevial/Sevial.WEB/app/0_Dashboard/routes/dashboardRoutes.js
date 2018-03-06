81b3b3d67fe6b4f5c6c9a7b1a7095b18c893e6ed
Andrés C <andresfccy@gmail.com>
2018-03-06 17:20:32 -0500

    Avances: Corrección de un nombre que se había ido mal puesto, Estructuración visual del login

---
+++ Sevial/Sevial.WEB/app/0_Dashboard/routes/dashboardRoutes.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+dashboardModule.config(['$stateProvider',
+    function ($stateProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/Home',
+                templateUrl: 'app/dashboard/views/dashboard.html',
+                resolve: {
+                    //"check": function (SessionServices, $location, loading, growl) {
+                    //    if (!SessionServices.isLoggedIn()) {
+                    //        $location.path("/Home");
+                    //        growl.info("¡Bienvenido de nuevo!");
+                    //    }
+                    //    SessionServices.authorized("login");
+                    //}
+                }
+            })
+    }
+]);
