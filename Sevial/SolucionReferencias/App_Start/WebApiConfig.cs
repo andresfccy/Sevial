cbb8474e492e1eff7281fa7780ce53af7a1b846c
Andrés C <andresfccy@gmail.com>
2018-03-06 13:50:02 -0500

    Login OK

--- Sevial/SolucionReferencias/App_Start/WebApiConfig.cs
+++ Sevial/SolucionReferencias/App_Start/WebApiConfig.cs
@@ -1,9 +1,9 @@
-﻿using Newtonsoft.Json.Serialization;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Formatting;
 using System.Web.Http;
 
-namespace Sevial
+namespace SolucionReferencias
 {
     public static class WebApiConfig
     {
@@ -16,12 +16,9 @@ namespace Sevial
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "{controller}/{id}",
+                routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-
-            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
-            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
