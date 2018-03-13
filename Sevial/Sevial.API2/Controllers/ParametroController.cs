d81e230dc405b5fef72566c0b96fee777023cd68
Hugo Armando Cendales Prieto <hugocendales@gmail.com>
2018-03-13 17:37:57 -0500

    Se incluye servicio DarListaCategoria

---
+++ Sevial/Sevial.API2/Controllers/ParametroController.cs
@@ -0,0 +1,45 @@
+﻿using Sevial.API2.Entity.Parametro;
+using Sevial.API2.Models.ObjetoEntrada.Parametro;
+using Sevial.API2.Models.ObjetoSalida;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Sevial.API2.Controllers
+{
+    public class ParametroController : ApiController
+    {
+        private APPSEVIALEntities2 db = new APPSEVIALEntities2();
+
+        [Route("api/parametro/darListaCategoria")]
+        [ResponseType(typeof(RespuestaLista<SP005_DarListaCategoria_Result>))]
+        public IHttpActionResult PostAutenticarUsuario(DarListaCategoria oe)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            ObjectParameter codigoRpta = new ObjectParameter("CodigoRpta", typeof(Int32));
+            ObjectParameter mensajeRpta = new ObjectParameter("MensajeRpta", typeof(String));
+
+            var result = db.SP005_DarListaCategoria(oe.AliasUsuario,oe.NomCategoria, oe.Filtro, codigoRpta, mensajeRpta);
+            var dataSet = result.ToList();
+
+            RespuestaLista<SP005_DarListaCategoria_Result> os = new RespuestaLista<SP005_DarListaCategoria_Result>();
+
+            os.CodigoRpta = Convert.ToInt32(codigoRpta.Value);
+            os.MensajeRpta = mensajeRpta.Value.ToString();
+            os.Lista = dataSet;
+
+            return Ok(os);
+
+
+        }
+    }
+}
---
+++ Sevial/Sevial.API2/Models/ObjetoEntrada/Parametro/DarListaCategoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sevial.API2.Models.ObjetoEntrada.Parametro
+{
+    public class DarListaCategoria : EntradaGeneral
+    {
+        public string NomCategoria { get; set; }
+        public string Filtro { get; set; }
+    }
+}
