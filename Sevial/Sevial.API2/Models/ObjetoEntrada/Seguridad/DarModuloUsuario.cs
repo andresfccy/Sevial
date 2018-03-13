570a66c914e806c67ee3aa91c6975609151afb3a
Hugo Armando Cendales Prieto <hugocendales@gmail.com>
2018-03-12 22:04:40 -0500

    Dar Modulo usuario

---
+++ Sevial/Sevial.API2/Models/ObjetoEntrada/Seguridad/DarModuloUsuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sevial.API2.Models.ObjetoEntrada.Seguridad
+{
+    public class DarModuloUsuario: EntradaGeneral
+    {
+    }
+}
