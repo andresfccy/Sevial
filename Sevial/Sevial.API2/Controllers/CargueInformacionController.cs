39a74c05315687b178ec99293951f7c3597dbac2
Hugo Armando Cendales Prieto <hugocendales@gmail.com>
2018-03-13 21:22:37 -0500

    Se incluye DarArchivosCargue

---
+++ Sevial/Sevial.API2/Controllers/CargueInformacionController.cs
@@ -0,0 +1,46 @@
+﻿using Sevial.API2.Entity.CargueInformacion;
+using Sevial.API2.Models.ObjetoEntrada.CargueInformacion;
+using Sevial.API2.Models.ObjetoSalida;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Sevial.API2.Controllers
+{
+    public class CargueInformacionController : ApiController
+    {
+
+        private APPSEVIALCargueInformacion db = new APPSEVIALCargueInformacion();
+
+        [Route("api/cargueInformacion/darArchivosCargue")]
+        [ResponseType(typeof(RespuestaLista<SP006_DarArchivosCargue_Result>))]
+        public IHttpActionResult PostDarArchivosCargue(DarArchivosCargue oe)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            ObjectParameter codigoRpta = new ObjectParameter("CodigoRpta", typeof(Int32));
+            ObjectParameter mensajeRpta = new ObjectParameter("MensajeRpta", typeof(String));
+
+            var result = db.SP006_DarArchivosCargue(oe.AliasUsuario, oe.IdTipoCargue, oe.IdEstadoCargue, codigoRpta, mensajeRpta);
+            var dataSet = result.ToList();
+
+            RespuestaLista<SP006_DarArchivosCargue_Result> os = new RespuestaLista<SP006_DarArchivosCargue_Result>();
+
+            os.CodigoRpta = Convert.ToInt32(codigoRpta.Value);
+            os.MensajeRpta = mensajeRpta.Value.ToString();
+            os.Lista = dataSet;
+
+            return Ok(os);
+
+
+        }
+    }
+}
